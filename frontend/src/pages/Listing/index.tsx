c47f51c9972103554e259786e0dd523ed1afe611
valmar Silva <valmar-ssilva@hotmail.com>
2022-03-13 20:00:41 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,29 @@
+import MovieCard from "components/MovieCard";
+
 import Pagination from "components/pagination";
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6col-lg-4colxl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6col-lg-4colxl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6col-lg-4colxl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6col-lg-4colxl-3  mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
